852902c25e1cfb0d1e5247960f7ef318a9063ae6
Nandeeshyr <68816986+Nandeeshyr@users.noreply.github.com>
2026-09-12 16:28:26 +0530

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.js
@@ -0,0 +1,27 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals) {
+        const time = intervals.sort((a,b)=>a.start - b.start)
+        for(let i=0; i<intervals.length-1; i++){
+            if(time[i].end <= time[i+1].start){
+                continue;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
